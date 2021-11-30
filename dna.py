b52895a4aa6b06b0353df8896fabb0d2f7d7bac0
Zebadiah S. Taylor <58185732+zebadiahtaylor@users.noreply.github.com>
2021-11-29 16:38:25 -0800

    Update dna.py

--- dna.py
+++ dna.py
@@ -5,7 +5,6 @@ import csv
 """
 written by zebadiah taylor for cs50 on 9/17/20 and is designed for the CS50 IDE.
 Reads through a found dna sequence (in .txt) and tests it against a known samples (in a .csv)
-Program is dynamic, and will adapt to any number of STR fields.
 
 test commands
 python dna.py databases/small.csv sequences/1.txt           Expected Output: Bob
@@ -21,40 +20,48 @@ if not len(argv) == 3:
 # opens dna databases to read
 with open(argv[1], newline='') as dna_db:
     reader = csv.DictReader(dna_db)
+    
     # opens the dna sequences
     with open(argv[2]) as sequence_reader:
         sequence = sequence_reader.read()
-    # we store # of max consecutive counts
+        
+    # stores # of max consecutive counts
     unknown_dna_sample = []
+    
     # outer loop begins by isolating the STRs to check, found via fieldnames in DictReader
     for x in range(len(reader.fieldnames)):
         # initializes or sets counters to 0
         str_count = 0
         max_str_count = 0
+        
         # lnitializes each STR here
         str_to_check = reader.fieldnames[x]
+        
         # excludes "name" from iterations
         if str_to_check != "name":
-            # let the looping begin!
+            
             # each loop goes letter by letter through the unidentified sample (sequence)
             for each_letter in range(len(sequence)):
-                # triggers when there's a STR match
-                # counts number of repeats
+                
+                # triggers when there's a STR match and counts repeats
                 while str_to_check == (sequence[each_letter:each_letter+len(str_to_check)]):
                     each_letter += len(str_to_check)
                     str_count += 1
+                    
                 # once STR stops repeating, saves new max count if appropriate.
                 if str_count > max_str_count:
                     max_str_count = str_count
                 # and then resets str_count
                 str_count = 0
+                
         # stores STR max_counts into a list to check with.
         unknown_dna_sample.append(max_str_count)
+        
     # var only used to output if no match is found.
     match_found = False
+    
     # Once the STR counts from the found DNA sequence have been read into unknown_dna_sample
     # iterates through DNA databases and checks values.
-
     for row in reader:
         str_count_match = 0
         for cell in range(len(reader.fieldnames)):
@@ -69,5 +76,6 @@ with open(argv[1], newline='') as dna_db:
             break
         else:
             str_count_match = 0
+            
     if match_found == False:
         print("No Match")
